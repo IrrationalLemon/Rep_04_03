95aaa042eed931f74465d6dc85734540b53b12d2
Polly <pavlikek2006@gmail.com>
2026-03-04 16:48:55 +0300

    Uploading app

---
+++ WpfApp5/WpfApp5/Models/ПунктВыдачи.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WpfApp5.Models;
+
+public partial class ПунктВыдачи
+{
+    public int Код { get; set; }
+
+    public string Адрес { get; set; } = null!;
+
+    public virtual ICollection<Заказ> Заказs { get; set; } = new List<Заказ>();
+}
